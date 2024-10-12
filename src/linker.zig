b7282fd104f477d2e09688dcd22536e8fe6acfcb
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-12 13:25:56 -0400

    changes to the linker
    * Finalizing linkUnOptimizedWithContext
    * added context options
    * make the test more comprehensive
    * reorganize the test

--- src/linker.zig
+++ src/linker.zig
@@ -24,6 +24,8 @@ const Parser = parser.Parser;
 const Value = parser.Value;
 const Node = parser.Node;
 
+pub const Error = error{MissingStart};
+
 /// ## Linking
 ///
 /// The linker struct defines methods to create usable binaries. These methods include headers, VASM headers, VASM
@@ -60,13 +62,28 @@ pub fn Linker(comptime binary_size: type) type {
 
         /// Populates the context's start function into the binary. `ctx` has to be a type
         /// that contains a `fold_procedures` boolean value, and a `start_definition` value.
+        ///
+        /// * `start_definition`
+        ///     * The name of the start function. If this function isn't encountered and `compile` isn't true that is an error.
+        /// * `compile`
+        ///     * Should the binary have no usage?
+        /// * `fold_procedures`
+        ///     * Should procedures be folded or should they be traditional?
+        ///         * **When disabling procedure folding, the first letter of the procedure name will be used. For faster and usable code, procedure folding should remain enabled.**
         pub fn linkUnOptimizedWithContext(self: *Self, ctx: anytype, proc_map: std.StringHashMap(std.ArrayList(binary_size))) !void {
             if (!ctx.fold_procedures) {
                 try self.iterateAndLink(ctx, proc_map);
             }
 
+            var entry_point_encountered: bool = false;
+
             if (proc_map.get(ctx.start_definition)) |start| {
                 try self.appendBytes(start.items[0..]);
+                entry_point_encountered = true;
+            }
+
+            if (!entry_point_encountered and ctx.compile == false) {
+                return error.MissingStart;
             }
         }
 
@@ -76,6 +93,7 @@ pub fn Linker(comptime binary_size: type) type {
             while (iterator_procedure_map.next()) |item| {
                 if (!std.mem.eql(u8, item.key_ptr.*, ctx.start_definition)) {
                     try self.appendByte(ctx.procedure_heading_byte);
+                    try self.appendByte(@bitCast(item.key_ptr.*[0]));
                     try self.appendBytes(item.value_ptr.items[0..]); // body of function
                     try self.appendByte(ctx.procedure_closing_byte);
                 }
@@ -87,6 +105,7 @@ pub fn Linker(comptime binary_size: type) type {
             try self.binary.appendSlice(bytes);
         }
 
+        /// Populates the binary with a given byte.
         pub fn appendByte(self: *Self, byte: binary_size) !void {
             try self.binary.append(byte);
         }
@@ -95,8 +114,8 @@ pub fn Linker(comptime binary_size: type) type {
 
 fn movInstructionTest(generator: *Generator(i8), vendor: *Vendor(i8), args: []Value) InstructionError!void {
     _ = vendor;
-    try generator.append(5);
 
+    try generator.append(5);
     try std.testing.expectEqual(0, args.len);
 }
 
@@ -133,6 +152,8 @@ test "creating and using a linker to create a usable binary" {
         .fold_procedures = false,
         .procedure_heading_byte = 10,
         .procedure_closing_byte = 22,
+        .compile = false,
+        .vasm_header = false,
     }, vend1.procedure_map);
 
     try std.testing.expectEqual(1, link.binary.items.len);
@@ -153,14 +174,18 @@ test "creating and using a linker to create a usable binary w/ procedures using 
 
     try vend1.implementInstruction("move", &mov_ins);
     try vend1.generateBinary(root);
+
     try link.linkUnOptimizedWithContext(.{
         .start_definition = "_start",
         .fold_procedures = false,
         .procedure_heading_byte = 10,
         .procedure_closing_byte = 22,
+        .compile = true,
+        .vasm_header = false,
     }, vend1.procedure_map);
-    try std.testing.expectEqual(3, link.binary.items.len);
+
+    try std.testing.expectEqual(4, link.binary.items.len);
     try std.testing.expectEqual(10, link.binary.items[0]);
-    try std.testing.expectEqual(5, link.binary.items[1]);
-    try std.testing.expectEqual(22, link.binary.items[2]);
+    try std.testing.expectEqual(5, link.binary.items[2]);
+    try std.testing.expectEqual(22, link.binary.items[3]);
 }
