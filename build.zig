e1801ad6217b0fc2111cee45b6acb2bafa86555a
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-12 13:12:18 -0400

    add build file

---
+++ build.zig
@@ -0,0 +1,36 @@
+const std = @import("std");
+
+pub fn build(builder: *std.Build) void {
+    const parser_unit_tests = builder.addTest(.{
+        .root_source_file = builder.path("src/parser.zig"),
+    });
+
+    const errors_unit_tests = builder.addTest(.{
+        .root_source_file = builder.path("src/errors.zig"),
+    });
+
+    const stylist_unit_tests = builder.addTest(.{
+        .root_source_file = builder.path("src/stylist.zig"),
+    });
+
+    const codegen_unit_tests = builder.addTest(.{
+        .root_source_file = builder.path("src/codegen.zig"),
+    });
+
+    const template_unit_tests = builder.addTest(.{
+        .root_source_file = builder.path("src/linker.zig"),
+    });
+
+    const build_step = builder.step("tests", "Runs and builds the test executable. --summary all gives a summary of completed and failed tests.");
+
+    // builder => build_step => depends on running lexer unit tests
+    //                                => and error unit tests
+    //                                => and stylist unit tests
+    // ...
+
+    build_step.dependOn(&builder.addRunArtifact(parser_unit_tests).step);
+    build_step.dependOn(&builder.addRunArtifact(errors_unit_tests).step);
+    build_step.dependOn(&builder.addRunArtifact(stylist_unit_tests).step);
+    build_step.dependOn(&builder.addRunArtifact(codegen_unit_tests).step);
+    build_step.dependOn(&builder.addRunArtifact(template_unit_tests).step);
+}
