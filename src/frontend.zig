deb1340f1d05654c0287775ec0e1e07a20cd49c7
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-14 22:38:35 -0400

    redesign frontend, add imports, still incomplete but usable

--- src/frontend.zig
+++ src/frontend.zig
@@ -4,63 +4,157 @@
 //!
 
 const std = @import("std");
+const drivers = @import("drivers.zig");
+const lexer = @import("lexer.zig");
+const parser = @import("parser.zig");
+const codegen = @import("codegen.zig");
+const linker = @import("linker.zig");
+const compiler = @import("compiler_main.zig");
+const compiler_output = @import("compiler_output.zig");
+const compiler_vendors = @import("compiler_vendors.zig");
+const stylist = @import("stylist.zig");
 
-/// Prints a bolded message in `format` using the `Writer.print` method.
-pub fn importantMessage(comptime format: []const u8, args: anytype) void {
-    const stdout = std.io.getStdOut().writer();
+const stringCompare = std.ascii.eqlIgnoreCase;
 
-    stdout.print("\x1b[1m", .{}) catch unreachable;
-    stdout.print(format, args) catch unreachable;
-    stdout.print("\x1b[0m", .{}) catch unreachable;
-    stdout.print("\n", .{}) catch unreachable;
+fn getOptions(allocator: anytype) compiler.Options {
+    const args = std.process.argsAlloc(allocator) catch {
+        compiler_output.errorMessage("failed to allocate a separate argument buffer. out of memory.", .{});
+        std.process.exit(1);
+    };
+
+    return compiler.extractOptions(allocator, args);
 }
 
-/// Prints an error message also using the `stderr.writer().print` function.
-pub fn errorMessage(comptime format: []const u8, args: anytype) void {
-    const stderr = std.io.getStdErr().writer();
+fn vendorStringToVendor(str: anytype) compiler_vendors.Tag {
+    if (stringCompare(str, "openlud")) {
+        return .openlud;
+    }
 
-    stderr.print("vasm: \x1b[31;1merror: \x1b[0;1m", .{}) catch unreachable;
-    stderr.print(format, args) catch unreachable;
-    stderr.print("\x1b[0m", .{}) catch unreachable;
-    stderr.print("\n", .{}) catch unreachable;
+    if (stringCompare(str, "nexfuse")) {
+        return .nexfuse;
+    }
+
+    if (stringCompare(str, "mercury")) {
+        return .mercury;
+    }
+
+    if (stringCompare(str, "solarisvm")) {
+        return .solarisvm;
+    }
+
+    if (stringCompare(str, "jade")) {
+        return .jade;
+    }
+
+    if (stringCompare(str, "siax")) {
+        return .siax;
+    }
+
+    return .unknown;
+}
+
+fn generateMethod(format: anytype, ctx: anytype) !void {
+    switch (format) {
+        .openlud => {
+            var gen = codegen.Vendor(i8).init(ctx.parent_allocator);
+            try drivers.openlud.vendor(&gen);
+
+            try gen.generateBinary(ctx.tree);
+
+            var link = linker.Linker(i8).init(ctx.parent_allocator);
+            link.linkOptimizedWithContext(drivers.openlud.ctx, &gen, gen.procedure_map) catch {
+                // switch (err) {
+                //     linker.Error.MissingStart => {
+                //         compiler_output.errorMessageWithExit("file `{s}` has no start method for format `{any}'.", .{ ctx.filepath, format });
+                //     },
+                // }
+                compiler_output.errorMessageWithExit("failed to link file '{s}'", .{ctx.file_name});
+            };
+
+            link.writeToFile(ctx.outfile, .little) catch {
+                compiler_output.errorMessage("error occured while writing to file `{s}'.", .{ctx.outfile});
+                std.process.exit(1);
+            };
+        },
+        else => {
+            if (format == .unknown) {
+                compiler_output.errorMessageWithExit("you must select a format with `--format' before compiling.  (see --format in the OPTIONS section)", .{});
+            }
+            compiler_output.errorMessageWithExit("format '{any}' is not currently supported.", .{format});
+        },
+    }
 }
 
 pub fn main() !void {
     var prog_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const prog_allocator = prog_arena.allocator();
-    const args = std.process.argsAlloc(prog_allocator) catch {
-        errorMessage("failed to allocate a separate argument buffer. out of memory.", .{});
-        std.process.exit(1);
-    };
-    var files = std.ArrayList([]const u8).init(prog_allocator);
 
-    var format: []const u8 = undefined;
+    const options = getOptions(prog_allocator);
 
-    var i: usize = 1;
+    // TODO: we need to also reserve room for the macro expander and runner
+    // TODO: as that's a very important part of the system. However, just not implemented YET (planned for v0.1.0)
+    var lex = lexer.Lexer.init(prog_allocator);
+    var pars = parser.Parser.init(prog_allocator, &lex.stream);
 
-    while (i < args.len) {
-        if (std.mem.eql(u8, args[i], "--format") or std.mem.eql(u8, args[i], "-f")) {
-            if (i + 1 >= args.len) {
-                errorMessage("'--format' expects a format argument.", .{});
-                std.process.exit(1);
-            } else {
-                i += 1;
-                format = args[i];
-            }
-        } else {
-            if (args[i][0] == '-') {
-                errorMessage("unrecognized flag '{s}'", .{args[i]});
-                std.process.exit(1);
-            } else {
-                try files.append(args[i]);
+    if (options.files.items.len == 0) {
+        compiler_output.errorMessageWithExit("no input files. (see -h or --help for more information)", .{});
+    }
+
+    for (options.files.items) |filename| {
+        const file = try std.fs.cwd().readFileAlloc(prog_allocator, filename, std.math.maxInt(usize));
+
+        lex.setInputText(file);
+
+        // STYLIST
+
+        if (options.stylist) {
+            const report = stylist.analyze(prog_allocator, file) catch {
+                compiler_output.errorMessageWithExit("could not run stylist(if this continues failing, try running vasm with --no-stylist)", .{});
+            };
+
+            for (report.items) |ding| {
+                compiler_output.importantMessage("{s}:{d}:{d}: ({any}) {s}", .{
+                    filename,
+                    ding.suggestion_location.line_number,
+                    ding.suggestion_location.problematic_area_begin,
+                    ding.suggestion_type,
+                    ding.suggestion_message,
+                });
+                if (ding.suggestion_location.line_number > 0) {
+                    compiler_output.getCustomarySourceLocationUsingLexer(
+                        &lex,
+                        ding.suggestion_location.problematic_area_begin,
+                        ding.suggestion_location.problematic_area_end,
+                        ding.suggestion_location.line_number - 1,
+                    );
+                }
             }
         }
 
-        i += 1;
-    }
+        lex.startLexingInputText() catch |err| compiler_output.printError(&lex, filename, err);
 
-    if (files.items.len == 0) {
-        errorMessage("no input files.", .{});
-        std.process.exit(1);
+        const ast = pars.createRootNode() catch |err| {
+            switch (err) {
+                error.EmptySubroutine => {
+                    compiler_output.errorMessage("{s}:{d}: Empty Subroutine", .{
+                        filename,
+                        0,
+                    });
+
+                    std.process.exit(1);
+                },
+                else => {
+                    std.process.exit(1);
+                },
+            }
+        };
+
+        const selected_vm = vendorStringToVendor(options.format.?);
+        try generateMethod(selected_vm, .{
+            .parent_allocator = prog_allocator,
+            .tree = ast,
+            .outfile = options.output,
+            .file_name = filename,
+        });
     }
 }
