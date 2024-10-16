44ff44525b8ea05f14951a379d3aaeb3414ddfcc
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-16 19:30:15 -0400

    refactor compile main and option getter

--- src/compiler_main.zig
+++ src/compiler_main.zig
@@ -8,8 +8,6 @@ const compiler_output = @import("compiler_output.zig");
 
 const ArrayList = std.ArrayList;
 
-const errorMessage = compiler_output.errorMessageWithExit;
-
 pub const Options = struct {
     files: ArrayList([]const u8),
     output: []const u8 = "a.out",
@@ -18,15 +16,15 @@ pub const Options = struct {
     strict_stylist: bool = false,
 };
 
-pub fn runManPage(allocator: anytype) void {
+pub fn runManPage(allocator: anytype, report: anytype) void {
     if (std.process.can_execv) {
         std.process.execv(allocator, &[_][]const u8{ "man", "vasm" }) catch {
-            errorMessage("no man installed.", .{});
+            report.errorMessage("no man installed.", .{});
         };
     }
 }
 
-pub fn extractOptions(allocator: std.mem.Allocator, arg_slice: [][:0]u8) Options {
+pub fn extractOptions(allocator: std.mem.Allocator, arg_slice: [][:0]u8, report: anytype) Options {
     var return_opt: Options = .{
         .output = "a.out",
         .format = "none",
@@ -39,27 +37,27 @@ pub fn extractOptions(allocator: std.mem.Allocator, arg_slice: [][:0]u8) Options
     while (i < arg_slice.len) {
         if (std.mem.eql(u8, arg_slice[i], "--format") or std.mem.eql(u8, arg_slice[i], "-f")) {
             if (i + 1 >= arg_slice.len) {
-                errorMessage("'--format' expects a format argument.", .{});
+                report.errorMessage("'--format' expects a format argument.", .{});
             } else {
                 i += 1;
                 return_opt.format = arg_slice[i];
             }
         } else if (std.mem.eql(u8, arg_slice[i], "--output") or std.mem.eql(u8, arg_slice[i], "-o")) {
             i += 1;
-            if (i >= arg_slice.len) errorMessage("-o/output expects an OUTFILE argument.", .{});
+            if (i >= arg_slice.len) report.errorMessage("-o/output expects an OUTFILE argument.", .{});
             return_opt.output = arg_slice[i];
         } else if (std.mem.eql(u8, arg_slice[i], "--help") or std.mem.eql(u8, arg_slice[i], "-h")) {
-            runManPage(allocator);
+            runManPage(allocator, report);
         } else if (std.mem.eql(u8, arg_slice[i], "--no-stylist")) {
             return_opt.stylist = false;
-        } else if (std.mem.eql(u8, arg_slice[i], "--enforce-stylist")) {
+        } else if (std.mem.eql(u8, arg_slice[i], "--strict") or std.mem.eql(u8, arg_slice[i], "--enforce-stylist")) {
             return_opt.strict_stylist = true;
         } else {
             if (arg_slice[i][0] == '-') {
-                errorMessage("unrecognized flag '{s}'", .{arg_slice[i]});
+                report.errorMessage("unrecognized flag '{s}'", .{arg_slice[i]});
             } else {
                 return_opt.files.append(arg_slice[i]) catch {
-                    errorMessage("Out of memory", .{});
+                    report.errorMessage("Out of memory", .{});
                 };
             }
         }
