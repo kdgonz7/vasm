c23d6f0beed010bbb34862cae155c48024e2b9c5
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-17 21:12:22 -0400

    change generate docs to find TODOs

--- todo.py
+++ todo.py
@@ -2,29 +2,32 @@ import os
 import sys
 
 def extractAllDocumentation(file: str):
-    doc_lines = []
+    todo_lines = []
 
     try:
         with open(file, "r") as f:
             lines = f.readlines()
 
             for line in lines:
-                if line.startswith("///") and line.strip() != "///":
-                    doc_lines.append(line.strip())
+                if line.find("TODO") != -1:
+                    todo_lines.append(line.strip())
+
     except OSError as error:
         print(f"error: failed to extract documentation because of error: {error}")
         sys.exit(1)
     
-    return doc_lines
+    return todo_lines
 
 for dir_path, dir_names, files in os.walk("."):
     for file in files:
         if file.endswith(".zig"):
 
            sub_path = os.path.join(dir_path, file)
-           docs = extractAllDocumentation(sub_path)
+           todos = extractAllDocumentation(sub_path)
+
+           if len(todos) <= 0: continue
 
            print(f"documentation in {sub_path}")
 
-           for doc_line in docs:
-               print(f"  {doc_line}")
+           for todo_line in todos:
+               print(f"  {todo_line}")
