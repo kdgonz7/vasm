bbfefe0662348b34c9abcd64f5b0f45b547e8df4
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-19 15:41:14 -0400

    fix a LOT of stylist errors

--- src/asm/helloworld.asm
+++ src/asm/helloworld.asm
@@ -2,5 +2,5 @@
 
 _start:
     zeroall ; clear all registers
-    lsl R1,'h','e','l','l','o',' ','w','o','r','l','d',0x0a
+    lsl R1, 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', 0x0a
     each R1 ; print out R1
