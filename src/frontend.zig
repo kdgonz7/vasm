7bc49549c49d04d59e4fbb8243bed39b3e520123
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-19 09:24:37 -0400

    organize all errors into the reporter class and remove the old main code

--- src/frontend.zig
+++ src/frontend.zig
@@ -17,6 +17,10 @@ const diagnostic = @import("stylist_diagnostic.zig");
 
 const stringCompare = std.ascii.eqlIgnoreCase;
 
+/// A binding to `compiler.extractOptions`,
+///
+/// Takes in an allocator and a reporter and passes those options into extractOptions, with the command
+/// line arguments allocated by `std.process.argsAlloc`.
 fn getOptions(allocator: anytype, reporter: *compiler_output.Reporter) compiler.Options {
     const args = std.process.argsAlloc(allocator) catch {
         reporter.errorMessage("failed to allocate a separate argument buffer. out of memory.", .{});
@@ -26,7 +30,8 @@ fn getOptions(allocator: anytype, reporter: *compiler_output.Reporter) compiler.
     return compiler.extractOptions(allocator, args, reporter);
 }
 
-fn vendorStringToVendor(str: anytype) compiler_vendors.Tag {
+/// Converts `str` into a `compiler_vendors.Tag`
+fn vendorStringToVendor(str: []const u8) compiler_vendors.Tag {
     if (stringCompare(str, "openlud")) {
         return .openlud;
     }
@@ -58,42 +63,18 @@ fn generateMethod(format: anytype, ctx: anytype) !void {
     switch (format) {
         .openlud => {
             var gen = codegen.Vendor(i8).init(ctx.parent_allocator);
+            var link = linker.Linker(i8).init(ctx.parent_allocator);
+
             try drivers.openlud.vendor(&gen);
 
-            gen.generateBinary(ctx.tree) catch |err| {
-                switch (err) {
-                    error.RegisterNumberTooLarge => {
-                        ctx.report.errorMessage("{s}:{d}:{d}: {s}", .{
-                            ctx.file_name,
-                            gen.erroneous_token.toRegister().span.line_number,
-                            gen.erroneous_token.toRegister().span.begin,
-                            "register number too large",
-                        });
+            // generate the procedure map
+            gen.generateBinary(ctx.tree) catch |err| ctx.report.genError(err, gen, ctx);
 
-                        ctx.report.getCustomarySourceLocationUsingLexer(
-                            ctx.lexer,
-                            gen.erroneous_token.toRegister().span.char_begin,
-                            gen.erroneous_token.toRegister().span.end,
-                            gen.erroneous_token.toRegister().span.line_number - 1,
-                        );
-                    },
-                    else => {
-                        return err;
-                    },
-                }
-            };
-
-            var link = linker.Linker(i8).init(ctx.parent_allocator);
-            link.linkOptimizedWithContext(drivers.openlud.ctx, &gen, gen.procedure_map) catch {
-                ctx.report.errorMessage("failed to link file '{s}'", .{ctx.file_name});
-                std.process.exit(1);
-            };
-
-            link.writeToFile(ctx.outfile, .little) catch {
-                ctx.report.errorMessage("error occured while writing to file `{s}'.", .{ctx.outfile});
-                std.process.exit(1);
-            };
+            // generate the optimized binary
+            link.linkOptimizedWithContext(drivers.openlud.ctx, &gen, gen.procedure_map) catch |err| ctx.report.linkerError(err, link, ctx);
+            link.writeToFile(ctx.outfile, .little) catch |err| ctx.report.linkerWriteError(err, link, ctx);
         },
+
         else => {
             if (format == .unknown) {
                 ctx.report.errorMessage("you must select a format with `--format' before compiling.  (see --format in the OPTIONS section)", .{});
@@ -105,123 +86,77 @@ fn generateMethod(format: anytype, ctx: anytype) !void {
     }
 }
 
-// pub fn main() !void {
-//     var prog_arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-//     const prog_allocator = prog_arena.allocator();
+fn checkNumberSizeFor(vm: compiler_vendors.Tag) usize {
+    switch (vm) {
+        .openlud,
+        => {
+            return std.math.maxInt(i8);
+        },
 
-//     const options = getOptions(prog_allocator);
-//     const stdout = std.io.getStdOut();
-//     const stderr = std.io.getStdErr();
+        .nexfuse,
+        => {
+            return std.math.maxInt(u8);
+        },
 
-//     const stdout_config = std.io.tty.detectConfig(stdout);
-//     const stderr_config = std.io.tty.detectConfig(stderr);
+        .siax => {
+            return std.math.maxInt(i32);
+        },
 
-//     // TODO: we need to also reserve room for the macro expander and runner
-//     // TODO: as that's a very important part of the system. However, just not implemented YET (planned for v0.1.0)
-//     var lex = lexer.Lexer.init(prog_allocator);
-//     var pars = parser.Parser.init(prog_allocator, &lex.stream);
+        .mercury => {
+            // TODO: mercury has experimental 32-bit support,
+            // however, the main support is 8-bit.
+            return std.math.maxInt(u8);
+        },
 
-//     if (options.files.items.len == 0) {
-//         compiler_output.errorMessageWithExit(stderr_config, "no input files. (see -h or --help for more information)", .{});
-//     }
+        .jade => {
+            // TODO: jade has options for 32-bit and 8-bit mode.
+            // this number is the greater half of the two.
+            return std.math.maxInt(u32);
+        },
 
-//     for (options.files.items) |filename| {
-//         const file = std.fs.cwd().readFileAlloc(prog_allocator, filename, std.math.maxInt(usize)) catch |err| {
-//             switch (err) {
-//                 error.FileNotFound => {
-//                     compiler_output.errorMessageWithExit(stderr_config, "could not locate file '{s}'", .{filename});
-//                 },
-//                 else => {
-//                     compiler_output.errorMessageWithExit(stderr_config, "something went wrong when reading file '{s}'. ({any})", .{ filename, err });
-//                 },
-//             }
-//         };
+        .solarisvm => {
+            // solarisvm is 32-bit.
+            return std.math.maxInt(u32);
+        },
 
-//         lex.setInputText(file);
+        else => @panic("unrecognized vm format"),
+    }
+}
 
-//         // STYLIST
+test checkNumberSizeFor {
+    try std.testing.expectEqual(checkNumberSizeFor(.openlud), std.math.maxInt(i8));
+    try std.testing.expectEqual(checkNumberSizeFor(.nexfuse), std.math.maxInt(u8));
+    try std.testing.expectEqual(checkNumberSizeFor(.jade), std.math.maxInt(u32));
+    try std.testing.expectEqual(checkNumberSizeFor(.siax), std.math.maxInt(i32));
+    try std.testing.expectEqual(checkNumberSizeFor(.solarisvm), std.math.maxInt(u32));
+    try std.testing.expectEqual(checkNumberSizeFor(.mercury), std.math.maxInt(u8));
+}
 
-//         if (options.stylist) {
-//             const report = stylist.analyze(prog_allocator, file) catch {
-//                 compiler_output.errorMessageWithExit(stderr_config, "could not run stylist(if this continues failing, try running vasm with --no-stylist)", .{});
-//             };
-
-//             for (report.items) |ding| {
-//                 compiler_output.importantMessage(stdout_config, "{s}:{d}:{d}: ({any}) {s}", .{
-//                     filename,
-//                     ding.suggestion_location.line_number,
-//                     ding.suggestion_location.problematic_area_begin,
-//                     ding.suggestion_type,
-//                     ding.suggestion_message,
-//                 });
-
-//                 if (ding.suggestion_location.line_number > 0) {
-//                     compiler_output.getCustomarySourceLocationUsingLexer(
-//                         &lex,
-//                         ding.suggestion_location.problematic_area_begin,
-//                         ding.suggestion_location.problematic_area_end,
-//                         ding.suggestion_location.line_number - 1,
-//                     );
-//                 }
-//             }
-
-//             if (report.items.len > 0 and options.strict_stylist) {
-//                 compiler_output.errorMessageWithExit("too many stylist errors, can not continue. (--enforce-stylist)", .{});
-//             }
-//         }
-
-//         lex.startLexingInputText() catch |err| compiler_output.printError(&lex, filename, err);
-
-//         const ast = pars.createRootNode() catch |err| {
-//             switch (err) {
-//                 error.EmptySubroutine => {
-//                     compiler_output.errorMessage("{s}:{d}: Empty Subroutine", .{
-//                         filename,
-//                         0,
-//                     });
-
-//                     std.process.exit(1);
-//                 },
-//                 else => {
-//                     std.process.exit(1);
-//                 },
-//             }
-//         };
-
-//         const selected_vm = vendorStringToVendor(options.format.?);
-//         try generateMethod(selected_vm, .{
-//             .parent_allocator = prog_allocator,
-//             .tree = ast,
-//             .outfile = options.output,
-//             .file_name = filename,
-//             .lexer = &lex,
-//         });
-//     }
-// }
-
-// TODO: if you're here from the compiler output file i want you (future me) to know that
-// this is looking pretty good, don't fuck up anything.
-// we just need to reimplement the commented out code modularly, which means
-// storing it all in functions and stuff. that makes the most sense. good luck.
-pub fn main() !void {
+/// Runs the standard VASM compiler.
+pub fn runCompilerFrontend() !void {
     var report = compiler_output.Reporter.init();
+
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    const allocator = arena.allocator();
     defer arena.deinit();
 
+    const allocator = arena.allocator();
+
+    // the command-line options.
     const opts = getOptions(allocator, &report);
 
+    // if there's no files, then exit
     if (opts.files.items.len == 0) {
         report.errorMessage("no input files", .{});
         std.process.cleanExit();
     }
 
+    // for each file, compile it
     for (opts.files.items) |file| {
         var lex = lexer.Lexer.init(allocator);
         var pars = parser.Parser.init(allocator, &lex.stream);
 
-        const file_body = std.fs.cwd().readFileAlloc(allocator, file, std.math.maxInt(usize)) catch {
-            report.errorMessage("could not create buffer for file '{s}`", .{file});
+        const file_body = std.fs.cwd().readFileAlloc(allocator, file, std.math.maxInt(usize)) catch |err| {
+            report.errorMessage("could not create buffer for file '{s}` ({any})", .{ file, err });
             std.process.exit(1);
         };
 
@@ -238,32 +173,19 @@ pub fn main() !void {
 
         const selected_vm = vendorStringToVendor(opts.format.?);
 
-        switch (selected_vm) {
-            .openlud,
-            => {
-                lex.rules.max_number_size = std.math.maxInt(i8);
-            },
-
-            .nexfuse,
-            => {
-                lex.rules.max_number_size = std.math.maxInt(u8);
-            },
-
-            .siax => {
-                lex.rules.max_number_size = std.math.maxInt(i32);
-            },
-
-            else => {},
+        if (selected_vm == .unknown) {
+            report.errorMessage("unknown format '{s}'", .{opts.format.?});
+            report.leaveNote("format enumerated to '{s}'", .{@tagName(selected_vm)});
+            std.process.exit(1);
         }
 
+        lex.rules.max_number_size = checkNumberSizeFor(selected_vm);
         lex.rules.check_for_big_numbers = !opts.allow_big_numbers;
+
         lex.startLexingInputText() catch |err| report.printError(&lex, file, err);
 
-        const ast = pars.createRootNode() catch |err| {
-            report.astError(err, lex);
-        };
+        const ast = pars.createRootNode() catch |err| report.astError(err, lex);
 
-        // generate using the selected VM
         try generateMethod(selected_vm, .{
             .parent_allocator = allocator,
             .tree = ast,
@@ -273,4 +195,8 @@ pub fn main() !void {
             .report = &report,
         });
     }
+}
+
+pub fn main() !void {
+    try runCompilerFrontend();
 }
