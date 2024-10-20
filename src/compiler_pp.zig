ada0fdfe286a297da2ed28d51847b316394b5896
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-19 22:24:49 -0400

    implement the default preprocessor runtime

---
+++ src/compiler_pp.zig
@@ -0,0 +1,23 @@
+//! ## Compiler Preprocessor
+//!
+//! This is a preprocessor for the VASM compiler.
+//!
+
+const std = @import("std");
+const preprocessor = @import("preprocessor.zig");
+
+const lexer = @import("lexer.zig");
+const parser = @import("parser.zig");
+
+const compiler_rt = @import("compiler-rt/root.zig");
+const compiler_main = @import("compiler_main.zig");
+
+pub fn preprocessWithDefaultRuntime(allocator: std.mem.Allocator, options: *compiler_main.Options, ast_root: *parser.Node) !preprocessor.PreprocessorResult {
+    var pp = preprocessor.Preprocessor.init(allocator, options);
+    defer pp.deinit();
+
+    try pp.addDirective("compat", &compiler_rt.compatDirective);
+    try pp.addDirective("endian", &compiler_rt.endianDirective);
+
+    return try pp.handleAstDirectives(ast_root);
+}
