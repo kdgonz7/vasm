1a293cd4046b26f7fd35eed02f574773bd5c694e
Kai D. Gonzalez <gkai70263@gmail.com>
2024-10-20 10:07:34 -0400

    add compile-if demonstration

---
+++ src/asm/compif.asm
@@ -0,0 +1,3 @@
+; COMPILEIF is a macro that exits if the program being compiled is being compiled under a different format.
+; essentially acts as the "compat" directive in VOLT and SIBC.
+[compile-if nexfuse]
